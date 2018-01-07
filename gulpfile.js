5097c3d75eb3528aa74024c914e23307a2cef66e
Rafael Bertelli <rafaelbertelli89@gmail.com>
2018-01-07 12:55:32 -0200

    Configurado SASS DEV e PROD

--- gulpfile.js
+++ gulpfile.js
@@ -4,51 +4,37 @@ const jshint 		= require('gulp-jshint')
 const concat 		= require('gulp-concat');
 const uglify 		= require('gulp-uglify');
 const rename 		= require('gulp-rename');
-const concatCss = require('gulp-concat-css');
+const uncomment	= require('gulp-strip-css-comments');
 
 /* SASS - Variables */
-var scssFiles = 'app/assets/css/scss/**/*.scss'; // Sass Source
-var cssDest = 'app/assets/dist/css'; // CSS destination
-var sassDevOptions = { outputStyle: 'expanded' } // Options for development
-var sassProdOptions = { outputStyle: 'compressed' } // Options for production
+const scssFiles			 	= 'app/assets/css/scss/**/*.scss'; 	// Sass Source
+const cssDest 				= 'app/assets/dist/css'; 						// CSS destination
+const sassDevOptions 	= { outputStyle: 'expanded' }; 			// Options for development
+const sassProdOptions = { outputStyle: 'compressed' }; 		// Options for production
+const cssRename				= 'estilo';													// Rename output file name
 
-// gulp.task('sassdev', function() {
-//   return gulp.src(scssFiles)
-// 		.pipe(sass(sassDevOptions).on('error', sass.logError))
-// 		.pipe(concatCss("estilo.css"))
-//     .pipe(gulp.dest(cssDest));
-// });
+gulp.task('sassdev', function() { // SASS task compiles any of our Sass files in our scss/ directory into CSS and saves the compiled CSS file in our dist/css directory.
+	return gulp.src(scssFiles)
+			.pipe(sass(sassDevOptions).on('error', sass.logError))
+			.pipe(rename(cssRename + ".css"))
+			.pipe(gulp.dest(cssDest));
+});
 
-// gulp.task('sassprod', function() {
-//   return gulp.src(scssFiles)
-// 		.pipe(sass(sassProdOptions).on('error', sass.logError))
-// 		// .pipe(concatCss("estilo.min.css"))
-// 		.pipe(rename('estilo.min.css'))
-//     .pipe(gulp.dest(cssDest));
-// });
+gulp.task('sassprod', function() {
+  return gulp.src(scssFiles)
+		.pipe(sass(sassProdOptions).on('error', sass.logError))
+		.pipe(uncomment({ all: true }))
+		.pipe(rename(cssRename + ".min.css"))
+    .pipe(gulp.dest(cssDest));
+});
 
-
-
-// Lint Task
-// Our lint task checks any JavaScript file in our js/ directory and makes sure there are no errors in our code.
-gulp.task('lint', function() {
+gulp.task('lint', function() { // Lint task checks any JavaScript file in our js/ directory and makes sure there are no errors in our code.
 	return gulp.src('app/assets/js/*.js')
 			.pipe(jshint())
 			.pipe(jshint.reporter('default'));
 });
 
-// Compile Our Sass
-// The sass task compiles any of our Sass files in our scss/ directory into CSS and saves the compiled CSS file in our dist/css directory.
-gulp.task('sass', function() {
-	return gulp.src('app/assets/css/scss/**/*.scss')
-			// .pipe(sass())
-			.pipe(sass())
-			.pipe(concatCss("estilo.css"))
-			.pipe(gulp.dest('app/assets/dist/css'));
-});
-
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
+gulp.task('scripts', function() { // Concatenate & Minify JS
 	return gulp.src('app/assets/js/*.js')
 			.pipe(concat('all.js'))
 			.pipe(gulp.dest('app/assets/dist/js'))
@@ -57,14 +43,10 @@ gulp.task('scripts', function() {
 			.pipe(gulp.dest('app/assets/dist/js'));
 });
 
-// Watch Files For Changes
-gulp.task('watch', function() {
-	gulp.watch('app/assets/js/*.js', ['lint', 'scripts']);
-	// gulp.watch('app/assets/css/scss/**/*.scss', ['sass', 'sassdev', 'sassprod']);
-	gulp.watch('app/assets/css/scss/**/*.scss', ['sass']);
+gulp.task('watch', function() { // Watch Files For Changes
+	gulp.watch( 'app/assets/js/*.js', ['lint', 'scripts'] );
+	gulp.watch( scssFiles, ['sassdev', 'sassprod'] );
 });
 
-// Default Task
-gulp.task('default',	['lint', 'sass', 'scripts', 'watch']);
-// gulp.task('default',	['lint', 'sass', 'sassdev', 'sassprod', 'scripts', 'watch']);
+gulp.task('default',	['sassdev', 'sassprod', 'lint', 'scripts', 'watch']); // Default Task
 
